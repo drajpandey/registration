025b0abcd99bdd4f2bb2ef2ba57500912f6e48bf
rajjpandey234 <rajjpandey234@gmail.com>
2023-06-09 15:17:14 +0530

    Changed local host to render server link

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -37,7 +37,7 @@ export default function Create() {
    // When a post request is sent to the create url, we'll add a new record to the database.
    const newPerson = { ...form,confirmation };
  
-   await fetch("http://localhost:5050/record", {
+   await fetch("https://registration-5m6t.onrender.com/record", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",
