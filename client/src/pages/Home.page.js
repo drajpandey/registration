a7c3af34f7943b391630a1c4d3157c130d587237
rajjpandey234 <rajjpandey234@gmail.com>
2023-07-13 12:37:33 +0530

    we r back with our design

--- client/src/pages/Home.page.js
+++ client/src/pages/Home.page.js
@@ -8,7 +8,7 @@ export default function Home() {
    
    <Navbar />
      <h1>Welcome to Our Site!!!</h1>
-     <h2>We have this site for our trip planning activity and we will add all our details in this website.</h2>
+     <h2>We have comeback here this site for our trip planning activity and we will add all our details in this website.</h2>
      <Homegrid />
    </>
  )
